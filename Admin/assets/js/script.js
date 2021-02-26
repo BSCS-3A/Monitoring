1d28e741621dc016a27cde25c6768b8f30def919
ayenrogero <ayenrogero@users.noreply.github.com>
2021-02-26 18:47:30 +0800

    on-going election

--- Admin/assets/js/script.js
+++ Admin/assets/js/script.js
@@ -31,3 +31,19 @@
           //seconds
         }, 0)
 }());
+
+move();
+function move(){
+  var elem = document.getElementById("p1");
+  var width = 10;
+  var id = setInterval(frame,39);
+  function frame(){
+    if (width >= 100){
+      clearInterval(id);
+    }else{
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width*1 + %;
+    }
+  }
+}
