3093a25e004e4ef03edf424b3e8cc96ec34e58f2
LowSpec-Coder-2nd <79438711+LowSpec-Coder-2nd@users.noreply.github.com>
2021-03-03 10:25:59 +0800

    Update db_connection.php

--- functionality_php/db_connection.php
+++ functionality_php/db_connection.php
@@ -1,7 +1,12 @@
 <?php
-
+/*
+* $sname stays the same
+*$uname should be change to the uname of db in the server
+*$password should also be change along with $db_name
+*only change when using it
+*/
 $sname= "localhost";
-$unmae= "root";
+$uname= "root";
 $password = "";
 
 $db_name = "db_monitoring";
