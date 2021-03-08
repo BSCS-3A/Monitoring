56cc3a7439957b8d103e1283400f9cf18453dcf3
mika <mikaellajarapa@gmail.com>
2021-03-08 11:24:18 +0800

    Update front_Monitoring_v8_0.php
    php code to connect to server and display the folders with year of the election

--- functionality_php/front_Monitoring_v8_0.php
+++ functionality_php/front_Monitoring_v8_0.php
@@ -1,21 +1,123 @@
-<?php
+<!--ELECTION ARCHIVES FOLDERS (ADMIN)-->
 
-//  Election Archives Folders (Admin)
+<!DOCTYPE html>
+<html>
 
-session_start();
-include "db_connection.php";
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta charset="utf-8">
+    <link rel="icon" href="..Admin/assets/img/buceils-logo.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="assets/js/countdown.js"></script>
+    <title>BUCEILS Voting System</title>
+</head>
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-//echo "Connected successfully";
+<body>
+    <nav>
+        <input id="nav-toggle" type="checkbox">
+        <div class="logo">
+            <h2>BUCEILS HS</h2>
+            <h3>ONLINE VOTING SYSTEM</h3>
+        </div>
+        <label for="btn" class="icon"><span class="fa fa-bars"></span></label>
+        <input type="checkbox" id="btn">
+        <ul>
+            <li>
+                <label for="btn-1" class="show">ACCOUNTS</label>
+                <a href="#">ACCOUNTS</a>
+                <input type="checkbox" id="btn-1">
+                <ul>
+                    <li><a href="#">Students</a></li>
+                    <li><a href="#">Admin</a></li>
+                </ul>
+            </li>
+            <li>
+                <label for="btn-2" class="show">ELECTION</label>
+                <a href="#">ELECTION</a>
+                <input type="checkbox" id="btn-2">
+                <ul>
+                    <li><a href="front_Monitoring_v8_0.php">Archive</a></li>
+                    <li><a href="front_Monitoring_v6_0.html ">Vote Status</a></li>
+                    <li><a href="front_Monitoring_v5_0.html">Vote Result</a>
+                        <ul>
+                            <li><a href="front_Monitoring_v10_0.html">Make Report</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Configuration</a>
+                    
+                </ul>
+            </li>
+            <li><a href="#">CANDIDATES</a></li>
+            <li>
+                <label for="btn-4" class="show">LOGS</label>
+                <a href="#">LOGS</a>
+                <input type="checkbox" id="btn-4">
+                <ul>
+                    <li><a href="#">Access Log</a></li>
+                    <li><a href="#">Activity Log</a></li>
+                    <li><a href="#">Vote Summary</a></li>
+                </ul>
+            </li>
+            <li><a href="#">MESSAGES</a></li>
+            <li>
+                <label for="btn-5" class="show">Admin Name</label>
+                <a class="user" href="#"><img class="user-profile" src="assets/img/user.png"></a>
+                <input type="checkbox" id="btn-5">
+                <ul>
+                    <li><a class="username" href="#">Admin Name</a></li>
+                    <li class="logout">
+                        <span class="fa fa-sign-out"></span><a href="#">LOGOUT</a></span>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <!--end of list-->
+    </nav>
 
-$result = mysqli_query($conn,"SELECT * FROM archive");
+        <div class="Barchives">
+          <p><b>ELECTION ARCHIVES</b></p>
+        </div>
+        
+          <?php
 
-$i=0;
-while($row = mysqli_fetch_array($result)) {
-    echo $row["school_year"];
-    echo "<br />";
-}
-?>
+            //  Election Archives Folders (Admin)
+
+            session_start();
+            include "db_connection.php";
+
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            //echo "Connected successfully";
+
+            $result1 = mysqli_query($conn,"SELECT YEAR(start_date) AS year FROM vote_event;");
+
+              while($row1 = mysqli_fetch_array($result1)) {
+                if(empty($row1['year'])){
+                  echo "no content";
+              }
+                else {
+                  include('folder.php');
+                }
+              }
+            ?>
+        <div class="footer">
+            <p class="footer-txt">BS COMPUTER SCIENCE 3A © 2021</p>
+        </div>
+    
+        <script>
+            $('.icon').click(function () {
+                $('span').toggleClass("cancel");
+            });
+            });
+            });
+        </script>
+   </body>
+    
+</html>
