53f955626c9b83518071cb1435bbaa3aa87423fa
mika <mikaellajarapa@gmail.com>
2021-04-21 23:03:15 +0800

    Edited UI of Results Report Page

--- functionality_php/admin/front_Report_v11_0.php
+++ functionality_php/admin/front_Report_v11_0.php
@@ -4,80 +4,90 @@
 <html>
 <html>
 <?php
-    // Connect database
-    include '../php/db_connection.php';
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    // Connect Election Results PDF
-    // include '../report/generate-pdf.php';
+// Connect database
+include '../php/db_connection.php';
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Connect Election Results PDF
+// include '../report/generate-pdf.php';
 ?>
+
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta charset="utf-8">
-    <link rel="icon" href="../Admin/assets/img/buceils-logo.png" type="image/png">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="assets/js/countdown.js"></script>
+    <link rel="icon" href="../../Admin/assets/img/BUHS LOGO.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="../../Admin/assets/css/style.css">
+    <link rel="stylesheet" href="../../Admin/assets/css/bootstrap4.5.2.css">
+    <link rel="stylesheet" href="../../Admin/assets/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="../../Admin/assets/css/font-awesome.css">
+    <link rel="stylesheet" href="../../Admin/assets/css/jquery.dataTables.min.css">
+    <!-- <script src="assets/js/a076d05399.js"></script> -->
+    <script src="../../Admin/assets/js/dataTables.bootstrap4.min.js"></script>
+    <script src="../../Admin/assets/js/jquery-3.5.1.js"></script>
+    <script src="../../Admin/assets/js/jquery.dataTables.min.js"></script>
+    <script src="../../Admin/assets/js/countdown.js"></script>
+    <script src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
     <title>BUCEILS Voting System</title>
 </head>
 
 <body>
     <nav>
-        <input id="nav-toggle" type="checkbox">
-        <div class="logo">
-            <h2>BUCEILS HS</h2>
-            <h3>ONLINE VOTING SYSTEM</h3>
+        <input class="nav-toggle1" type="checkbox">
+        <div class="aLogo">
+            <h2 class="aLogo-txt1"><a href="adminDashboard.html">BUCEILS HS</a></h2>
+            <h3 class="aLogo-txt2"><a href="adminDashboard.html">ONLINE VOTING SYSTEM</a></h3>
         </div>
-        <label for="btn" class="icon"><span class="fa fa-bars"></span></label>
-        <input type="checkbox" id="btn">
+        <label for="btn" class="ADicon"><span class="fa fa-bars"></span></label>
+        <input class="nav-toggle2" type="checkbox" id="btn">
         <ul>
             <li>
-                <label for="btn-1" class="show">ACCOUNTS</label>
+                <label for="btn-1" class="Ashow">ACCOUNTS</label>
                 <a href="#">ACCOUNTS</a>
-                <input type="checkbox" id="btn-1">
+                <input class="nav-toggle3" type="checkbox" id="btn-1">
                 <ul>
                     <li><a href="#">Students</a></li>
                     <li><a href="#">Admin</a></li>
                 </ul>
             </li>
             <li>
-                <label for="btn-2" class="show">ELECTION</label>
+                <label for="btn-2" class="Ashow">ELECTION</label>
                 <a href="#">ELECTION</a>
-                <input type="checkbox" id="btn-2">
+                <input class="nav-toggle4" type="checkbox" id="btn-2">
                 <ul>
                     <li><a href="front_ArchFolder_v8_0.php">Archive</a></li>
-                    <li><a href="#">Vote Status</a></li>
-                    <li><a href="#">Vote Result</a>
+                    <li><a href="front_VsPercentage_v6_0.php">Vote Status</a></li>
+                    <li><a href="front_Election_v5_0.php">Vote Result</a>
                         <ul>
-                            <li><a href="#">Make Report</a></li>
+                            <li><a href="../../functionality_php/report/generate-pdf.php">Make Report</a></li>
                         </ul>
                     </li>
                     <li><a href="#">Configuration</a>
-                    
+                        <ul>
+                            <li><a href="#">Scheduler</a></li>
+                            <li><a href="#">Signatory</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </li>
             <li><a href="#">CANDIDATES</a></li>
             <li>
-                <label for="btn-4" class="show">LOGS</label>
+                <label for="btn-4" class="Ashow">LOGS</label>
                 <a href="#">LOGS</a>
-                <input type="checkbox" id="btn-4">
+                <input class="nav-toggle5" type="checkbox" id="btn-4">
                 <ul>
-                    <li><a href="#">Access Log</a></li>
+                    <li><a href="accessLogs-v2.0.html">Access Log</a></li>
                     <li><a href="#">Activity Log</a></li>
                     <li><a href="#">Vote Summary</a></li>
                 </ul>
             </li>
             <li><a href="#">MESSAGES</a></li>
             <li>
-                <label for="btn-5" class="show">Admin Name</label>
-                <a class="user" href="#"><img class="user-profile" src="assets/img/user.png"></a>
-                <input type="checkbox" id="btn-5">
+                <label for="btn-5" class="Ashow">Admin Name</label>
+                <a class="user" href="#"><img class="user-profile" src="../../Admin/assets/img/user.png"></a>
+                <input class="nav-toggle6" type="checkbox" id="btn-5">
                 <ul>
                     <li><a class="username" href="#">Admin Name</a></li>
                     <li class="logout">
@@ -89,60 +99,59 @@
         <!--end of list-->
     </nav>
 
-        <div class="Barchives">
-          <p><b>ELECTION RESULTS REPORT</b></p>
-        </div>
-        
-        <?php
-        //  Election Results Report (Admin)
-        
-          	//----------GETS THE WINNER PER POSITION
-                //Count number of candidate_position
-                    $result = mysqli_query($conn,"SELECT * FROM candidate_position");
-                    $positionCount = mysqli_num_rows($result);
-                    // echo "$positionCount";
-                //Get highest vote per candidate_position
-					for($i=1; $i<=$positionCount; $i++)
-					{
-						$rowSQL = mysqli_query($conn, "SELECT MAX(total_votes) AS tempWinner FROM candidate WHERE position_id = '$i'");
-						list($max) = mysqli_fetch_row($rowSQL);
-						echo "$i = $max;\n";
-					}//end for loop
+    <div class="Breport">
+        <p><b>ELECTION RESULTS REPORT</b></p>
+    </div>
 
-                //Store id of candidate with highest vote
-					// $names[$i] = mysqli_fetch_row($rowSQL);
-					// echo " = $names['total_votes'];\n";
+    <?php
+    //  Election Results Report (Admin)
 
-            //----------CHECKS FOR TIE
-                //If there is more than 1 highest vote per candidate_position
-                //Prompt to pick
-                //Store id of candidate votes given a +1
-                //Update stored id of winning candidate
+    //----------GETS THE WINNER PER POSITION
+    //Count number of candidate_position
+    $result = mysqli_query($conn, "SELECT * FROM candidate_position");
+    $positionCount = mysqli_num_rows($result);
+    // echo "$positionCount";
+    //Get highest vote per candidate_position
+    for ($i = 1; $i <= $positionCount; $i++) {
+        $rowSQL = mysqli_query($conn, "SELECT MAX(total_votes) AS tempWinner FROM candidate WHERE position_id = '$i'");
+        list($max) = mysqli_fetch_row($rowSQL);
+        echo "$i = $max;\n";
+    } //end for loop
 
-            //----------CHECKS IF WINNER MET MINIMUM VOTE QUOTA
-                //At least 50% of all votes for non-representative positions
-                    //If not, update stored id to zero or null
-                //At least 50% of year-level votes for representative positions
-                    //If not, update stored id to zero or null
-        ?>
+    //Store id of candidate with highest vote
+    // $names[$i] = mysqli_fetch_row($rowSQL);
+    // echo " = $names['total_votes'];\n";
 
-        <div class="Bbtn_post">
-            <button onclick="parent.open('http://localhost/Monitoring-main/functionality_php/report/generate-pdf.php')" class="Bbtn_postresults scs-responsive"><b>DOWNLOAD PDF</b></button>
-        </div>
+    //----------CHECKS FOR TIE
+    //If there is more than 1 highest vote per candidate_position
+    //Prompt to pick
+    //Store id of candidate votes given a +1
+    //Update stored id of winning candidate
+
+    //----------CHECKS IF WINNER MET MINIMUM VOTE QUOTA
+    //At least 50% of all votes for non-representative positions
+    //If not, update stored id to zero or null
+    //At least 50% of year-level votes for representative positions
+    //If not, update stored id to zero or null
+    ?>
+
+    <div class="Bbtn_post">
+        <button onclick="parent.open('http://localhost/Monitoring-main/functionality_php/report/generate-pdf.php')" class="Bbtn_postresults scs-responsive"><b>DOWNLOAD PDF</b></button>
+    </div>
 
 
-        <!-- Space before footer -->
-        <br><br><br><br><br><br><br><br>
+    <!-- Space before footer -->
+    <br><br><br><br><br><br><br><br>
 
-        <div class="footer">
-            <p class="footer-txt">BS COMPUTER SCIENCE 3A © 2021</p>
-        </div>
-    
-        <script>
-            $('.icon').click(function () {
-                $('span').toggleClass("cancel");
-            });
-        </script>
-   </body>
-    
+    <div class="footer">
+        <p class="footer-txt">BS COMPUTER SCIENCE 3A © 2021</p>
+    </div>
+
+    <script>
+        $('.icon').click(function() {
+            $('span').toggleClass("cancel");
+        });
+    </script>
+</body>
+
 </html>
