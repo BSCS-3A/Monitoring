a4dfcf9af3f2a515be3fbb96b966edb50cfd6daf
mika <mikaellajarapa@gmail.com>
2021-04-24 23:25:56 +0800

    Update position_vote_count.php
    Select right sum of total votes of candidates per position

--- functionality_php/php/position_vote_count.php
+++ functionality_php/php/position_vote_count.php
@@ -1,14 +1,18 @@
 <?php
     require 'db_connection.php';   
     
-    $query = "SELECT position_id, SUM(total_votes) as votes_per_position FROM candidate GROUP BY position_id ORDER BY position_id";
+    $query = "SELECT c.position_id as position, cp.heirarchy_id as hierarchy_order, sum(c.total_votes) as votes_per_position
+    FROM candidate c
+    LEFT JOIN candidate_position cp ON c.position_id=cp.position_id
+    GROUP BY position
+    ORDER BY position";
 
     $result = mysqli_query($conn, $query);
     
     $position_votes = array();
 	  while($vote=mysqli_fetch_array($result)){
       $position_votes[] = array( 
-        "position_id"=>$vote['position_id'],
+        "position"=>$vote['position_id'],
         "votes_per_position"=>$vote['votes_per_position']
       );
     }
