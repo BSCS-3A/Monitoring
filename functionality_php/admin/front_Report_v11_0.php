d5c3326926a2fe8f9c2f3eb58ead96e9243b27ed
lizpen <79520632+lizpen@users.noreply.github.com>
2021-04-26 21:50:25 +0800

    replaced nav with navAdmin.php
    file copied from mainline.
    shortened the code using functions

--- functionality_php/admin/front_Report_v11_0.php
+++ functionality_php/admin/front_Report_v11_0.php
@@ -2,152 +2,154 @@
 
 <!DOCTYPE html>
 <html>
-<html>
-<?php
-    // Connect database
-    include '../php/db_connection.php';
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-?>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta charset="utf-8">
+        <link rel="icon" href="../../Admin/assets/img/BUHS LOGO.png" type="image/png">
+        <link rel="stylesheet" type="text/css" href="../../Admin/assets/css/style.css">
+        <link rel="stylesheet" href="../../Admin/assets/css/bootstrap4.5.2.css">
+        <link rel="stylesheet" href="../../Admin/assets/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" href="../../Admin/assets/css/font-awesome.css">
+        <link rel="stylesheet" href="../../Admin/assets/css/jquery.dataTables.min.css">
+        <script src="../../Admin/assets/js/dataTables.bootstrap4.min.js"></script>
+        <script src="../../Admin/assets/js/jquery-3.5.1.js"></script>
+        <script src="../../Admin/assets/js/jquery.dataTables.min.js"></script>
+        <script src="../../Admin/assets/js/countdown.js"></script>
+        <script src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
+        <title>BUCEILS Voting System</title>
+    </head>
 
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta charset="utf-8">
-    <link rel="icon" href="../../Admin/assets/img/BUHS LOGO.png" type="image/png">
-    <link rel="stylesheet" type="text/css" href="../../Admin/assets/css/style.css">
-    <link rel="stylesheet" href="../../Admin/assets/css/bootstrap4.5.2.css">
-    <link rel="stylesheet" href="../../Admin/assets/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../Admin/assets/css/font-awesome.css">
-    <link rel="stylesheet" href="../../Admin/assets/css/jquery.dataTables.min.css">
-    <!-- <script src="assets/js/a076d05399.js"></script> -->
-    <script src="../../Admin/assets/js/dataTables.bootstrap4.min.js"></script>
-    <script src="../../Admin/assets/js/jquery-3.5.1.js"></script>
-    <script src="../../Admin/assets/js/jquery.dataTables.min.js"></script>
-    <script src="../../Admin/assets/js/countdown.js"></script>
-    <script src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
-    <title>BUCEILS Voting System</title>
-</head>
+    <body>
+        <?php
+            include '../php/db_connection.php';                     // Connect database
+            if ($conn->connect_error)                               // Check connection
+                die("Connection failed: " . $conn->connect_error);
+            include '../php/fetch_date.php';                        // Connect fetch_date
+            include "navAdmin.php";                                 // Add the navBar
 
-<body>
-    <nav>
-        <input class="nav-toggle1" type="checkbox">
-        <div class="aLogo">
-            <h2 class="aLogo-txt1"><a href="adminDashboard.html">BUCEILS HS</a></h2>
-            <h3 class="aLogo-txt2"><a href="adminDashboard.html">ONLINE VOTING SYSTEM</a></h3>
+            // Function to get quota
+            function getQuota($total)
+            {
+                if ( $total > 0 ){
+                    return (int) ( ($total/2)+1 );
+                } else {
+                    return 0;
+                }
+            } // end getQuota
+
+            // Function to check tie
+            function checkTie($con, $idNumber, $maxVotes)
+            {
+                $tieQuery = mysqli_query($con, "SELECT * FROM candidate WHERE position_id = '$idNumber' AND total_votes='$maxVotes'");
+                return mysqli_num_rows($tieQuery);
+            } // end checkTie
+
+            // Function to INSERT to archive
+            function insertToArchive($con)
+            {
+                $con->query("INSERT INTO `archive` (`archive_id`, `position_name`, `winner_fname`, `winner_mname`, `winner_lname`, `school_year`, `platform_info`) VALUES (NULL, 'cc', 'gd', 'ffss', 'iuf', CURRENT_DATE(), 'yef')");
+            }
+
+            // Function to get winners
+            function getWinners($con, $res, $arrAdd, $count, $maxVt)
+            {
+                if($maxVt>=getQuota($res[$arrAdd]))
+                {//At least 50% of whole population
+                    if(checkTie($con, $count, $maxVt)>=2){
+                        // echo " SEMI WIN ";
+                        // TIE BREAKER
+                        // if headadmin
+                        if($_SESSION['admin_position'] == "Head Admin"){
+                            // echo "{Pick one}";
+                        }else{
+                            // echo "{Can't pick}";
+                        }
+                        //else if ordinary admin
+                    }else{
+                        // echo "  SURE WIN  ";
+                        insertToArchive($con);
+                    }
+
+                    // THEN, add to archive
+                }else{
+                    // echo "  -- Didn't Met Quota --  ";
+                }
+            }
+
+        
+    ?>
+
+        <div class="Breport">
+            <p><b>ELECTION RESULTS REPORT</b></p>
         </div>
-        <label for="btn" class="ADicon"><span class="fa fa-bars"></span></label>
-        <input class="nav-toggle2" type="checkbox" id="btn">
-        <ul>
-            <li>
-                <label for="btn-1" class="Ashow">ACCOUNTS</label>
-                <a href="#">ACCOUNTS</a>
-                <input class="nav-toggle3" type="checkbox" id="btn-1">
-                <ul>
-                    <li><a href="#">Students</a></li>
-                    <li><a href="#">Admin</a></li>
-                </ul>
-            </li>
-            <li>
-                <label for="btn-2" class="Ashow">ELECTION</label>
-                <a href="#">ELECTION</a>
-                <input class="nav-toggle4" type="checkbox" id="btn-2">
-                <ul>
-                    <li><a href="front_ArchFolder_v8_0.php">Archive</a></li>
-                    <li><a href="front_VsPercentage_v6_1.php">Vote Status</a></li>
-                    <li><a href="front_Election_v5_0">Vote Result</a>
-                        <ul>
-                            <li><a href="../../functionality_php/report/generate-pdf.php">Make Report</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Configuration</a>
-                        <ul>
-                            <li><a href="#">Scheduler</a></li>
-                            <li><a href="#">Signatory</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="#">CANDIDATES</a></li>
-            <li>
-                <label for="btn-4" class="Ashow">LOGS</label>
-                <a href="#">LOGS</a>
-                <input class="nav-toggle5" type="checkbox" id="btn-4">
-                <ul>
-                    <li><a href="accessLogs-v2.0.html">Access Log</a></li>
-                    <li><a href="#">Activity Log</a></li>
-                    <li><a href="#">Vote Summary</a></li>
-                </ul>
-            </li>
-            <li><a href="#">MESSAGES</a></li>
-            <li>
-                <label for="btn-5" class="Ashow">Admin Name</label>
-                <a class="user" href="#"><img class="user-profile" src="../../Admin/assets/img/user.png"></a>
-                <input class="nav-toggle6" type="checkbox" id="btn-5">
-                <ul>
-                    <li><a class="username" href="#">Admin Name</a></li>
-                    <li class="logout">
-                        <span class="fa fa-sign-out"></span><a href="#">LOGOUT</a></span>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <!--end of list-->
-    </nav>
 
-    <div class="Breport">
-        <p><b>ELECTION RESULTS REPORT</b></p>
-    </div>
-
-    <?php
-        //  Election Results Report (Admin)
-
-            //----------GETS THE WINNER PER POSITION
-                //Count number of candidate_position
-                $result = mysqli_query($conn,"SELECT * FROM candidate_position");
-                $positionCount = mysqli_num_rows($result);
-                // echo "$positionCount";
-                
-                for($i=1; $i<=$positionCount; $i++)
+        <?php
+            // Count and store to Archive right after election
+                if($current_date_time>$last_election_date)//change to automatically compute after election
                 {
-                    $rowSQL = mysqli_query($conn, "SELECT MAX(total_votes) AS tempWinner FROM candidate WHERE position_id = '$i'"); //Get highest vote per candidate_position
-                    list($max) = mysqli_fetch_row($rowSQL);
-                    // echo "$i = $max;\n";
+                    //----------GETS THE WINNER PER POSITION
+                    //Count number of candidate_position
+                        $result = mysqli_query($conn,"SELECT * FROM candidate_position");
+                        $positionCount = mysqli_num_rows($result);
 
-                    // Check if there is a tie for tentative winners
-                        //If there is more than 1 highest vote per candidate_position
-                        //Prompt to pick
+                    // Counts year-level and overall student
+                        $queryPopulation=mysqli_query($conn, "SELECT sum(case when grade_level = '7' then 1 else 0 end) AS g7,
+                                        sum(case when grade_level = '8' then 1 else 0 end) AS g8,
+                                        sum(case when grade_level = '9' then 1 else 0 end) AS g9,
+                                        sum(case when grade_level = '10' then 1 else 0 end) AS g10,
+                                        sum(case when grade_level = '11' then 1 else 0 end) AS g11,
+                                        sum(case when grade_level = '12' then 1 else 0 end) AS g12,
+                                        count(student_id) AS totalPopulation FROM student");
+                    // Stores year-level and overall student population
+                        $res = mysqli_fetch_array($queryPopulation);
 
-                    // Check if winner meets minimum quota
-                        //At least 50% of all votes for non-representative positions
-                            //Then, add to archive
-                        //At least 50% of year-level votes for representative positions
-                            //Then, add to archive
+                    for($i=1; $i<=$positionCount; $i++)
+                    {
+                        // Count the highest vote per position
+                            $rowSQL = mysqli_query($conn, "SELECT MAX(total_votes) AS tempWinner FROM candidate WHERE position_id = '$i'");
+                            list($max) = mysqli_fetch_row($rowSQL);
+                        // echo "$i = $max";
 
-                }//end for loop
+                        if($max>0)
+                        {
+                            $voteAllow=mysqli_fetch_array($result);
+                            if($voteAllow['vote_allow']==1)
+                            {// For non-representative positions
+                                getWinners($conn, $res, '6', $i, $max);
+                            }else{// For representative positions
+                                // Get the candidate's year level
+                                    $gradeQuery = mysqli_query($conn,"SELECT candidate.position_id, candidate.student_id, student.student_id, student.grade_level FROM candidate INNER JOIN student ON candidate.student_id = student.student_id WHERE candidate.position_id = '$i'");
+                                    $gradeLevel=mysqli_fetch_array($gradeQuery);
+                                getWinners($conn, $res, ($gradeLevel['grade_level'] - 7), $i, $max);
+                            } //end if else
+                        }
+            ?>
+                        <br>
+            <?php
+                    }//end for loop
+                }
+                else{
+                    echo "Election is still ongoing\n";
+                }
 
+                // Free all query if everything is resolved
+                    
+            ?>
                 
-        ?>
 
-    <div class="Bbtn_post">
-        <button onclick="parent.open('http://localhost/Monitoring-main/functionality_php/report/generate-pdf.php')" class="Bbtn_postresults scs-responsive"><b>DOWNLOAD PDF</b></button>
-    </div>
+        <div class="Bbtn_post">
+            <button onclick="parent.open('http://localhost/Monitoring-main/functionality_php/report/generate-pdf.php')" class="Bbtn_postresults scs-responsive"><b>DOWNLOAD PDF</b></button>
+        </div>
 
 
-    <!-- Space before footer -->
-    <br><br><br><br><br><br><br><br>
+        <!-- Space before footer -->
+        <br><br><br><br><br><br><br><br>
 
-    <div class="footer">
-        <p class="footer-txt">BS COMPUTER SCIENCE 3A © 2021</p>
-    </div>
-
-    <script>
-        $('.icon').click(function() {
-            $('span').toggleClass("cancel");
-        });
-    </script>
-</body>
-
+        <script>
+            $('.icon').click(function() {
+                $('span').toggleClass("cancel");
+            });
+        </script>
+    </body>
 </html>
