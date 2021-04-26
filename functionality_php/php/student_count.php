2e171d8308a610705ed20b8b5a8855736ee148c7
lizpen <79520632+lizpen@users.noreply.github.com>
2021-04-27 00:35:56 +0800

    Update student_count.php
    Added query used for generate-pdf and front_Report

--- functionality_php/php/student_count.php
+++ functionality_php/php/student_count.php
@@ -38,4 +38,17 @@
         $total_students += $record['students_voted'];
     }
 
+
+    // STUDENT POPULATION COUNTERS USED IN front_Report_v11_0.php AND generate-pdf.php
+        // Counts per year-level and overall enrolled student
+            $queryEnrolled=mysqli_query($conn, "SELECT sum(case when grade_level = '7' then 1 else 0 end) AS g7,
+                sum(case when grade_level = '8' then 1 else 0 end) AS g8,
+                sum(case when grade_level = '9' then 1 else 0 end) AS g9,
+                sum(case when grade_level = '10' then 1 else 0 end) AS g10,
+                sum(case when grade_level = '11' then 1 else 0 end) AS g11,
+                sum(case when grade_level = '12' then 1 else 0 end) AS g12,
+                count(student_id) AS totalEnrolled FROM student");
+            $enrolled = mysqli_fetch_array($queryEnrolled);
+            // accessed through: $enrolled[0] --> g7, $enrolled[1] --> g8 ... $enrolled[6] --> total
+
 ?>
