0addfbd33e989e1a6515511b31aa6b54d51228b9
lizpen <79520632+lizpen@users.noreply.github.com>
2021-04-28 00:46:52 +0800

    Update fetch_report.php
    Changed conditions and updated functions

--- functionality_php/php/fetch_report.php
+++ functionality_php/php/fetch_report.php
@@ -1,62 +1,67 @@
 <?php
+	// Function that returns election results status
+		// function isResultFinal($status)
+		// {
+		// 	return $status;
+		// }
+
 	// Function to get quota
         function getQuota($total)
         {
-            if ( $total > 0 ){
+            if ( $total > 0 )
                 return (int) ( ($total/2)+1 );
-            } else {
+            else 
                 return 0;
-            }
         } // end getQuota
 
-    // Function to check tie
-        function checkTie($con, $idNumber, $maxVotes)
+    // Function to get number of tied winning candidates per position
+        function numTie($conn, $i, $max)
         {
-            $tieQuery = mysqli_query($con, "SELECT * FROM candidate WHERE position_id = '$idNumber' AND total_votes='$maxVotes'");
-            return mysqli_num_rows($tieQuery);
-        } // end checkTie
+            return mysqli_num_rows($conn->query("SELECT * FROM candidate WHERE position_id = '$i' AND total_votes='$max'"));
+        } // end numTie
 
-    // Function to INSERT to archive
-        function insertToArchive($con)
-        {
-            $con->query("INSERT INTO `archive` (`archive_id`, `position_name`, `winner_fname`, `winner_mname`, `winner_lname`, `school_year`, `platform_info`) VALUES (NULL, 'cc', 'gd', 'ffss', 'iuf', CURRENT_DATE(), 'yef')");
+        function getTiedCandidates($conn, $i, $max, & $tiedCandidates){
+        	$tieQuery = $conn->query("SELECT * FROM candidate WHERE position_id = '$i' AND total_votes='$max'");
+        	for ($k=0; $k < numTie($conn, $i, $max); $k++) {
+        		$tiedStudents= mysqli_fetch_array($tieQuery);
+            	$tiedCandidates = $tiedCandidates." || candId = ".$tiedStudents['candidate_id']."";
+            }
         }
 
-    // Function to get winners
-        function getWinners($con, $res, $arrAdd, $count, $maxVt)
+    // Function to INSERT to archive
+        function insertToArchive($conn,$winnerCandidates)
         {
-            if($maxVt>=getQuota($res[$arrAdd]))
-            {//At least 50% of whole population
-                if(checkTie($con, $count, $maxVt)>=2){
-                    echo " SEMI WIN ";
-                    // TIE BREAKER
-                    // if headadmin
-                    if($_SESSION['admin_position'] == "Head Admin"){
-                        echo "{Pick one}";
-                    }else{
-                        echo "{Can't pick}";
-                    }
-                    //else if ordinary admin
-	            }else{
-	                echo "  SURE WIN  ";
-	                insertToArchive($con);
-	            }
+        	for($n=0; $n<sizeof($winnerCandidates); $n++)
+        	{
+        		$conn->query("INSERT INTO `archive` (`archive_id`, `position_name`, `winner_fname`, `winner_mname`, `winner_lname`, `school_year`, `platform_info`) VALUES (NULL, 'cc', 'gd', 'ffss', 'iuf', CURRENT_DATE(), 'yef')");
+        	}
+        }
 
-                // THEN, add to archive
+    // Function to get lists of tied candidates and winners
+        function getLists($conn, $enrolled, $arrAdd, $i, $max, & $tiedCandidates, & $winnerCandidates)
+        {
+        	if($max>=getQuota($enrolled[$arrAdd]))
+            {//At least 50% of whole population
+                if(numTie($conn, $i, $max)>=2){
+                    // $resultStatus = false;
+                    getTiedCandidates($conn, $i, $max, $tiedCandidates);
+	            }else{
+	                $winnerStudents= mysqli_fetch_array($conn->query("SELECT * FROM candidate WHERE position_id = '$i' AND total_votes='$max'"));
+		            array_push($winnerCandidates, (int)$winnerStudents['candidate_id']);
+
+	            }
             }else{
                 echo "  -- Didn't Met Quota --  ";
             }
         }
 
-        function getGradeLevel($con, $id)
+        function getGradeLevel($conn, $i)
         {
-        	$gradeQuery = mysqli_query($con,"SELECT candidate.position_id, candidate.student_id, student.student_id, student.grade_level FROM candidate INNER JOIN student ON candidate.student_id = student.student_id WHERE candidate.position_id = '$id'");
-            $gradeLevel=mysqli_fetch_array($gradeQuery);
+        	$gradeLevel=mysqli_fetch_array($conn->query("SELECT candidate.position_id, candidate.student_id, student.student_id, student.grade_level FROM candidate INNER JOIN student ON candidate.student_id = student.student_id WHERE candidate.position_id = '$i'"));
             return ($gradeLevel['grade_level'] - 7);
         }
 
     //Count number of candidate_position
         $result = mysqli_query($conn,"SELECT * FROM candidate_position");
         $positionSize = mysqli_num_rows($result);
-
 ?>
