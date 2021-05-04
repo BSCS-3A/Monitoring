fb2b791ddb3ef4522f0255d6212aa14dbcea3159
lizpen <79520632+lizpen@users.noreply.github.com>
2021-05-04 20:26:53 +0800

    Add query for generate-pdf.php

--- functionality_php/php/student_count.php
+++ functionality_php/php/student_count.php
@@ -51,4 +51,14 @@
             $enrolled = mysqli_fetch_array($queryEnrolled);
             // accessed through: $enrolled[0] --> g7, $enrolled[1] --> g8 ... $enrolled[6] --> total
 
+        // Counts per year-level and overall voted student
+            $queryVoted=mysqli_query($conn, "SELECT sum(case when grade_level = '7' then voting_status end) AS g7Voted,
+                    sum(case when grade_level = '8' then voting_status end) AS g8Voted,
+                    sum(case when grade_level = '9' then voting_status end) AS g9Voted,
+                    sum(case when grade_level = '10' then voting_status end) AS g10Voted,
+                    sum(case when grade_level = '11' then voting_status end) AS g11Voted,
+                    sum(case when grade_level = '12' then voting_status end) AS g12Voted,
+                    count(student_id) AS totalVoted FROM student");
+            $studVoted = mysqli_fetch_array($queryVoted);
+
 ?>
